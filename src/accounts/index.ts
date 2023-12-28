0771596927966ce2a8f8af752ab97e3e5c940e7a
Aviral Agarwal <43955898+Aviral1506@users.noreply.github.com>
2023-12-28 18:54:31 +0530

    added default class for accounts

---
+++ src/accounts/index.ts
@@ -0,0 +1,8 @@
+import { networkId } from "../constants";
+
+export class Accounts {
+    printHelloWorld(): void {
+      console.log("Hello, World!");
+      console.log(`network Id ${networkId}`)
+    }
+  }git s
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { Accounts } from "./accounts";
+
+const myAccount = new Accounts();
+myAccount.printHelloWorld();
