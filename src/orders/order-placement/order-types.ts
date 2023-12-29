42a9e96d92e8512e0f187ca115ba635c99c5cee4
Kunal Jha <106550721+Kun-J@users.noreply.github.com>
2023-12-29 09:30:39 +0530

    comments

--- src/orders/order-placement/order-types.ts
+++ src/orders/order-placement/order-types.ts
@@ -1,6 +1,15 @@
-class PerpOrder {
-  constructor() {}
+class order {}
 
+class PerpOrder extends order {
+  /**
+   * Places a limit order in the perpetual market.
+   * @param quantity - The quantity of the order.
+   * @param price - The price at which the order is placed.
+   * @param inputToken - The token to be used as input in the order.
+   * @param outputToken - The token to be received as output in the order.
+   * @param direction - The direction of the order, e.g., "buy" or "sell".
+   * @param leverage - The leverage applied to the order.
+   */
   limit(
     quantity: number,
     price: number,
@@ -12,6 +21,14 @@ class PerpOrder {
     console.log("Limit order placed");
   }
 
+  /**
+   * Places a market order in the perpetual market.
+   * @param quantity - The quantity of the order.
+   * @param inputToken - The token to be used as input in the order.
+   * @param outputToken - The token to be received as output in the order.
+   * @param direction - The direction of the order, e.g., "buy" or "sell".
+   * @param leverage - The leverage applied to the order.
+   */
   market(
     quantity: number,
     inputToken: string,
@@ -22,6 +39,13 @@ class PerpOrder {
     console.log("Market order placed");
   }
 
+  /**
+   * Places a conditional limit order in the perpetual market.
+   * @param triggerPrice - The price that triggers the order.
+   * @param executionPrice - The price at which the order is executed.
+   * @param direction - The direction of the order, e.g., "buy" or "sell".
+   * @param leverage - The leverage applied to the order.
+   */
   conditionalLimit(
     triggerPrice: number,
     executionPrice: number,
@@ -31,6 +55,14 @@ class PerpOrder {
     console.log("Conditional Limit order placed");
   }
 
+  /**
+   * Places a conditional market order in the perpetual market.
+   * @param triggerPrice - The price that triggers the order.
+   * @param direction - The direction of the order, e.g., "buy" or "sell".
+   * @param position - The desired position of the order.
+   * @param quantity - The quantity of the order.
+   * @param leverage - The leverage applied to the order.
+   */
   conditionalMarket(
     triggerPrice: number,
     direction: string,
@@ -41,6 +73,13 @@ class PerpOrder {
     console.log("Conditional Market order placed");
   }
 
+  /**
+   * Places a stop loss order in the perpetual market.
+   * @param triggerPrice - The price that triggers the stop loss.
+   * @param quantity - The quantity of the order.
+   * @param direction - The direction of the order, e.g., "buy" or "sell".
+   * @param leverage - The leverage applied to the order.
+   */
   stop(
     triggerPrice: number,
     quantity: number,
@@ -50,29 +89,50 @@ class PerpOrder {
     console.log("Stop loss order placed");
   }
 
+  /**
+   * Places a trailing price order in the perpetual market based on a price difference.
+   * @param diff - The price difference used for trailing.
+   * @param direction - The direction of the order, e.g., "buy" or "sell".
+   * @param quantity - The quantity of the order.
+   * @param leverage - The leverage applied to the order.
+   */
   trailByPriceDiff(
     diff: number,
     direction: string,
     quantity: number,
     leverage: number
   ): void {
-    //This is according to Price Difference
     console.log("Trailing Price order placed");
   }
 
+  /**
+   * Places a trailing order in the perpetual market based on a percentage difference.
+   * @param diff - The percentage difference used for trailing.
+   * @param direction - The direction of the order, e.g., "buy" or "sell".
+   * @param quantity - The quantity of the order.
+   * @param leverage - The leverage applied to the order.
+   */
   trailByDiffPerc(
     diff: number,
     direction: string,
     quantity: number,
     leverage: number
   ): void {
-    //This is according to Percentage Difference
     console.log("Trailing order placed");
   }
 }
 
-class SpotOrder {
-  constructor() {}
+/**
+ * Represents a class for Spot Orders in a trading system.
+ */
+class SpotOrder extends order {
+  /**
+   * Places a limit order in the spot market.
+   * @param quantity - The quantity of the order.
+   * @param price - The price at which the order is placed.
+   * @param inputToken - The token to be used as input in the order.
+   * @param outputToken - The token to be received as output in the order.
+   */
   limit(
     quantity: number,
     price: number,
@@ -82,14 +142,31 @@ class SpotOrder {
     console.log("Limit order placed");
   }
 
+  /**
+   * Places a market order in the spot market.
+   * @param quantity - The quantity of the order.
+   * @param inputToken - The token to be used as input in the order.
+   * @param outputToken - The token to be received as output in the order.
+   */
   market(quantity: number, inputToken: string, outputToken: string): void {
     console.log("Market order placed");
   }
 
+  /**
+   * Places a conditional limit order in the spot market.
+   * @param triggerPrice - The price that triggers the order.
+   * @param executionPrice - The price at which the order is executed.
+   */
   conditionalLimit(triggerPrice: number, executionPrice: number): void {
     console.log("Conditional Limit order placed");
   }
 
+  /**
+   * Places a conditional market order in the spot market.
+   * @param triggerPrice - The price that triggers the order.
+   * @param position - The desired position of the order.
+   * @param quantity - The quantity of the order.
+   */
   conditionalMarket(
     triggerPrice: number,
     position: boolean,
@@ -98,17 +175,30 @@ class SpotOrder {
     console.log("Conditional Market order placed");
   }
 
+  /**
+   * Places a stop loss order in the spot market.
+   * @param triggerPrice - The price that triggers the stop loss.
+   * @param quantity - The quantity of the order.
+   */
   stop(triggerPrice: number, quantity: number): void {
     console.log("Stop loss order placed");
   }
 
+  /**
+   * Places a trailing price order in the spot market based on a price difference.
+   * @param diff - The price difference used for trailing.
+   * @param quantity - The quantity of the order.
+   */
   trailByPriceDiff(diff: number, quantity: number): void {
-    //This is according to Price Difference
     console.log("Trailing Price order placed");
   }
 
+  /**
+   * Places a trailing order in the spot market based on a percentage difference.
+   * @param diff - The percentage difference used for trailing.
+   * @param quantity - The quantity of the order.
+   */
   trailByDiffPerc(diff: number, quantity: number): void {
-    //This is according to Percentage Difference
     console.log("Trailing order placed");
   }
 }
